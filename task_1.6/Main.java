b1bf72b9ec317173dd6bc81886f09596d807ec24
darkscout90 <82582015+darkscout90@users.noreply.github.com>
2021-04-15 16:59:19 +0300

    Add files via upload

---
+++ task_1.6/Main.java
@@ -0,0 +1,16 @@
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+		Scanner scan;
+		scan = new Scanner(System.in);
+	    int a = scan.nextInt();
+	    double b;
+	    b=a * 1.15;
+		System.out.println(b);
+    }
+}
