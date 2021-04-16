5ea0854624f1014eba4a2c8a29a6761b69d3be22
darkscout90 <82582015+darkscout90@users.noreply.github.com>
2021-04-16 12:29:12 +0300

    Add files via upload

---
+++ task_2.1/Main.java
@@ -0,0 +1,19 @@
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String a="Мама";
+	String b="Мыла";
+	String c="Раму";
+		System.out.println(a+b+c);
+		System.out.println(a+c+b);
+		System.out.println(b+a+c);
+		System.out.println(b+c+a);
+		System.out.println(c+a+b);
+		System.out.println(c+b+a);
+    }
+}
