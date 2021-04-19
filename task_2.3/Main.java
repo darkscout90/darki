e61fd41d2dd12d4e9bc96f79a3ba9f490a570777
darkscout90 <82582015+darkscout90@users.noreply.github.com>
2021-04-19 13:30:44 +0300

    Add files via upload

---
+++ task_2.3/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str="8";
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(str);
+            str+="8";
+            
+        }
+    }
+}
