7897baecb125c282f8b63717ceff1d4142f6c645
darkscout90 <82582015+darkscout90@users.noreply.github.com>
2021-04-21 15:55:10 +0300

    Add files via upload

---
+++ task_3.3/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int a=4;
+    int b=4;
+        System.out.println(fun1(a,b));
+    }
+    static boolean fun1(int a,int b){
+        boolean flag=false;
+        if (a==b)flag = true;
+        return flag;
+    }
+
+}
